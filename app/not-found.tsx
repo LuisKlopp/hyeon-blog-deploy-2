53f191f329e082c9ef5bc3c84a939b81e33692f6
LuisKlopp <fbgus333@gmail.com>
2024-03-05 14:34:06 +0900

    Feat: not-found.tsx 생성

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <span className="text-2xl">존재하지 않는 페이지입니다!</span>
+    </div>
+  );
+};
+
+export default NotFound;
