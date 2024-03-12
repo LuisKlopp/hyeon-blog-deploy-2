e7ea5a9d3a1054b1a124a56e9602284a6ccf1cd3
LuisKlopp <fbgus333@gmail.com>
2024-03-12 20:17:11 +0900

    Feat: mdx-components 생성 및 post 상세페이지 생성

---
+++ components/mdx-components.tsx
@@ -0,0 +1,21 @@
+import * as runtime from "react/jsx-runtime";
+
+const useMDXComponent = (code: string) => {
+  const fn = new Function(code);
+  return fn({ ...runtime }).default;
+};
+
+const components = {
+  Image,
+};
+
+interface MdxProps {
+  code: string;
+}
+
+export const MDXContent = ({
+  code,
+}: MdxProps) => {
+  const Component = useMDXComponent(code);
+  return <Component components={components} />;
+};
