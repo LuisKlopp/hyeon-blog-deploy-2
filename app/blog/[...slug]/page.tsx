c986ce2904ef330ef7fa7acb8bdd2e4203d88b19
LuisKlopp <fbgus333@gmail.com>
2024-03-18 17:23:57 +0900

    Feat: nextjs14 세팅 블로그 글 포스팅

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -40,8 +40,8 @@ const PostPage = async ({
   }
 
   return (
-    <article className="container py-6 prose dark:prose-invert max-w-3xl mx-auto">
-      <h1 className="mb-2">{post.title}</h1>
+    <article className="container py-6 pb-32 prose dark:prose-invert max-w-3xl mx-auto">
+      <h1 className="mt-2">{post.title}</h1>
       {post.description ? (
         <p className="text-xl mt-0 text-muted-foreground">
           {post.description}
