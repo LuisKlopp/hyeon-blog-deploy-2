50ab1de6d3f9e73ca01a0366a1e1255da359dd3b
LuisKlopp <fbgus333@gmail.com>
2024-03-21 01:47:10 +0900

    Feat: next.config.mjs 변경

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
-import { build } from "velite";
-
 /** @type {import('next').NextConfig} */
-
 export default {
-  reactStrictMode: true,
-  swcMinify: true,
+  // othor next config here...
   webpack: (config) => {
     config.plugins.push(
       new VeliteWebpackPlugin(),
@@ -15,15 +11,10 @@ export default {
 
 class VeliteWebpackPlugin {
   static started = false;
-  constructor(
-    /** @type {import('velite').Options} */ options = {},
-  ) {
-    this.options = options;
-  }
   apply(
     /** @type {import('webpack').Compiler} */ compiler,
   ) {
-    // executed three times in nextjs !!!
+    // executed three times in nextjs
     // twice for the server (nodejs / edge runtime) and once for the client
     compiler.hooks.beforeCompile.tapPromise(
       "VeliteWebpackPlugin",
@@ -32,11 +23,8 @@ class VeliteWebpackPlugin {
         VeliteWebpackPlugin.started = true;
         const dev =
           compiler.options.mode === "development";
-        this.options.watch =
-          this.options.watch ?? dev;
-        this.options.clean =
-          this.options.clean ?? !dev;
-        await build(this.options); // start velite
+        const { build } = await import("velite");
+        await build({ watch: dev, clean: !dev });
       },
     );
   }
