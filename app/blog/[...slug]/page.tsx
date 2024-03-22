29e023048a45ba42e061804670a44bb169b0048d
LuisKlopp <fbgus333@gmail.com>
2024-03-22 16:46:28 +0900

    Refactor: blog page의 api og 제거

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -43,22 +43,6 @@ export async function generateMetadata({
       description: post.description,
       type: "article",
       url: post.slug,
-      images: [
-        {
-          url: `/api/og?${ogSearchParams.toString()}`,
-          width: 1200,
-          height: 630,
-          alt: post.title,
-        },
-      ],
-    },
-    twitter: {
-      card: "summary_large_image",
-      title: post.title,
-      description: post.description,
-      images: [
-        `/api/og?${ogSearchParams.toString()}`,
-      ],
     },
   };
 }
